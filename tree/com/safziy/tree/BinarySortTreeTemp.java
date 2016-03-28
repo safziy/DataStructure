d2b26da2dd383695fef02e517dbb8b7479d4d0a8
g <safziy@foxmail.com>
2016-03-28 15:14:44 +0800

    Commit

---
+++ tree/com/safziy/tree/BinarySortTreeTemp.java
@@ -0,0 +1,66 @@
+package com.safziy.tree;
+
+public class BinarySortTreeTemp extends BinaryTree {
+
+	@Override
+	public void add(int t) {
+		if (root == null) {
+			root = new Node(t);
+			return;
+		}
+		Node node = root;
+		while (true) {
+			if (t < node.t) {
+				if (node.left == null) {
+					node.left = new Node(t);
+					return;
+				}
+				node = node.left;
+			} else if (t > node.t) {
+				if (node.right == null) {
+					node.right = new Node(t);
+					break;
+				}
+				node = node.right;
+			}else{
+				// the same element
+				return;
+			}
+		}
+
+	}
+
+	@Override
+	public boolean remove(int t) {
+		Node removeNode = root;
+		Node parent = root;
+		while(removeNode != null){
+			if (t < removeNode.t) {
+				removeNode = removeNode.left;
+			} else if (t > removeNode.t) {
+				removeNode = removeNode.right;
+			}else{
+				break;
+			}
+			parent = removeNode;
+		}
+		if(removeNode == null){
+			// not exits the element
+			return false;
+		}
+		
+		if(removeNode.left == null && removeNode.right == null){
+			if(parent.left == removeNode){
+				parent.left = null;
+			}else if(parent.right == removeNode){
+				parent.right = null;
+			}else{
+				root = null;
+			}
+		}
+		
+		
+		return false;
+	}
+
+}
